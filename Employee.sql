6808f60d73882115ab480c3ca9ee26a87a62a069
Syarif Hidayat <Syarif_H657@mitrais.com>
2019-01-30 14:04:46 +0700

    add method for handling CLOB

--- Employee.sql
+++ Employee.sql
@@ -1,5 +1,7 @@
-CREATE TABLE `employee` (
+select * from employee;CREATE TABLE `employee` (
   `id` int(10) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
-  `salary` int(10) DEFAULT NULL
+  `photo` longblob,
+  `salary` int(10) DEFAULT NULL,
+  `working_history` longtext
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
